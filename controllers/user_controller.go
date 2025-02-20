3779d129370bbb9e7a79711dea685a10cd8924bb
farhanzeb95 <farhanzeb95@gmail.com>
2025-02-20 12:56:43 +0100

    addec crud operations

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,8 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
+	"go-web-service/models"
+	"io"
 	"net/http"
 	"regexp"
+	"strconv"
 )
 
 type userController struct {
@@ -10,11 +15,115 @@ type userController struct {
 }
 
 func (userController userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from user controller"))
+	if r.URL.Path == "/users" {
+		switch r.Method {
+		case http.MethodGet:
+			userController.getAllUsers(w, r)
+		case http.MethodPost:
+			userController.AddNewUser(w, r)
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
+		}
+	} else {
+		matches := userController.userIdPattern.FindStringSubmatch(r.URL.Path)
+		fmt.Println(matches)
+
+		if len(matches) < 2 { // Ensure at least two elements before accessing matches[1]
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		id, err := strconv.Atoi(matches[1])
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		switch r.Method {
+		case http.MethodGet:
+			userController.getUserById(id, w)
+		case http.MethodPut:
+			userController.UpdateUser(id, w, r)
+		case http.MethodDelete:
+			panic("method not implemented")
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
+		}
+	}
 }
 
 func newUserController() *userController {
 	return &userController{
-		userIdPattern: regexp.MustCompile(`^/users/(/d)/?`),
+		userIdPattern: regexp.MustCompile(`^/users/(\d+)/?$`), // Corrected regex
 	}
 }
+
+func (userController userController) getAllUsers(w http.ResponseWriter, r *http.Request) {
+	encodeResponseAsJSON(models.GetUsers(), w)
+}
+
+func encodeResponseAsJSON(data interface{}, w io.Writer) {
+	encodedJson := json.NewEncoder(w)
+
+	encodedJson.Encode((data))
+}
+
+func (userController userController) getUserById(id int, w http.ResponseWriter) {
+	fmt.Println(id)
+	user, error := models.GerUserById(id)
+
+	if error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	encodeResponseAsJSON(user, w)
+}
+
+func (userController userController) AddNewUser(w http.ResponseWriter, r *http.Request) {
+	user, error := parseRequest(r)
+
+	if error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not pasrse the object"))
+	}
+
+	user, error = models.AddUser(user)
+
+	if error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(error.Error()))
+
+		return
+	}
+
+	encodeResponseAsJSON(user, w)
+
+}
+
+func parseRequest(r *http.Request) (models.User, error) {
+	decoder := json.NewDecoder((r.Body))
+	var u models.User
+
+	error := decoder.Decode(&u)
+
+	if error != nil {
+		return models.User{}, error
+	}
+
+	return u, nil
+
+}
+
+func (userController userController) UpdateUser(id int, w http.ResponseWriter, r *http.Request) {
+	panic("method not implemented")
+}
+
+func (userController userController) RemoveUser(w http.ResponseWriter, r *http.Request) {
+	panic("method not implemented")
+}
+
+// func newUserController() *userController {
+// 	return &userController{
+// 		userIdPattern: regexp.MustCompile(`^/users/(/d)/?`),
+// 	}
+// }
--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	ID        int
 	FirstName string
@@ -16,10 +21,46 @@ func GetUsers() []*User {
 }
 
 func AddUser(user User) (User, error) {
+	if user.ID != 0 {
+		return User{}, errors.New("The new user must not contain any id")
+	}
 	user.ID = nextId
 	nextId++
 
 	users = append(users, &user)
 
 	return user, nil
+}
+
+func GerUserById(id int) (User, error) {
+	for _, user := range users {
+		if user.ID == id {
+			return *user, nil
+		}
+	}
+	return User{}, fmt.Errorf("The user with id '%v' does not exist", id)
+}
+
+func UpdatedUserById(userToUpdate User) (User, error) {
+	for i, user := range users {
+		if user.ID == userToUpdate.ID {
+			users[i] = &userToUpdate
+
+			return userToUpdate, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("The user with id '%v' does not exist", userToUpdate.ID)
+}
+
+func RemoveUserByID(id int) ([]User, error) {
+	for i, user := range users {
+		if user.ID == id {
+			users = append(users[:i], users[i+1:]...)
+
+			return nil, nil
+		}
+	}
+
+	return []User{}, fmt.Errorf("The user with id '%v' does not exist", id)
 }
